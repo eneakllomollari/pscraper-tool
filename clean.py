d8a9e095db8f39174b07cb2c7bbd0e65f3887493
Enea <eneakllomollari@gmail.com>
2020-06-30 00:07:07 -0700

    Update clean script

--- clean.py
+++ clean.py
@@ -1,21 +1,21 @@
-import os
+from MySQLdb import connect
 
-import MySQLdb
-
-db = MySQLdb.connect(
-        host='35.236.114.231',
-        user='pscraper',
-        db='pscraper_db',
-        password='G6y5eiaXe8euyVC'
-     )
+db = connect(
+    host='35.236.114.231',
+    user='pscraper',
+    db='pscraper_db',
+    password='G6y5eiaXe8euyVC'
+)
 cursor = db.cursor()
-cursor.execute('select group_concat(id) from pscraper_history group by vin, price, seller_id having (count(vin)>1)')
+cursor.execute('''
+select group_concat(id) from pscraper_history 
+group by vin, price, seller_id,mileage, marketplace 
+having (count(vin)>1)
+''')
 for r in cursor.fetchall():
-    for id in r[0].split(',')[1:]:
-        print(id)
-        if id:
+    for id_ in r[0].split(',')[1:]:
+        if id_:
             cursor.execute(f'DELETE FROM pscraper_history WHERE id={id}')
             db.commit()
-
 db.commit()
 db.close()
